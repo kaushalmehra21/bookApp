7e691a125663bc36d38b41caed88dd782bda5135
kaushalmehra21 <kaushalmehra@live.com>
2018-01-30 17:35:13 +0530

    add some more tables

---
+++ database/migrations/2018_01_30_111051_create_taxes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTaxesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('taxes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->double('tax', 2, 2)->default(0);
+            $table->string('title', 50);
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('modified_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('taxes');
+    }
+}
