32777b9d1c5a412c1a999fb3620b8b5fa99842e3
kaushalmehra21 <kaushalmehra@live.com>
2018-02-24 15:36:58 +0530

    change folder stucture of document

---
+++ documents/front-end/top_bar.php
@@ -0,0 +1,37 @@
+		<div class="topbar">
+			<div class="container">
+				
+				<!-- Online Option -->
+				<div class="online-option">
+					<ul>
+						<li><a href="#">online store</a></li>
+						<li><a href="#">Payment</a></li>
+						<li><a href="#">shipping</a></li>
+						<li><a href="#">Return</a></li>
+					</ul>
+				</div>
+				<!-- Online Option -->
+
+				<!-- Social Icons -->
+				<div class="social-icons pull-right">
+					<ul>
+						<li><a class="fa fa-facebook" href="#"></a></li>	
+						<li><a class="fa fa-twitter" href="#"></a></li>	
+						<li><a class="fa fa-google-plus" href="#"></a></li>	
+						<li><a class="fa fa-pinterest-p" href="#"></a></li>	
+					</ul>
+				</div>
+				<!-- Social Icons -->
+
+				<!-- Cart Option -->
+				<div class="cart-option">
+					<ul>
+						<li class="add-cart"><a href="#"><i class="fa fa-shopping-bag"></i><span>3</span></a></li>
+						<li><a href="#"><i class="fa fa-heart-o"></i>wish List 0 items</a></li>
+						<li><a href="#" data-toggle="modal" data-target="#login-modal"><i class="fa fa-user"></i>Login / Register</a></li>
+					</ul>
+				</div>
+				<!-- Cart Option -->
+
+			</div>
+		</div>
